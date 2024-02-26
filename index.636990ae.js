27755b765160a3ff066a9f449740d22f5c5b5be8
TarasKachmar2022 <107129371+TarasKachmar2022@users.noreply.github.com>
2024-02-26 23:46:50 +0000

    Deploying to gh-pages from @ TarasKachmar2022/FinanceLedger@d430284c4a222e8256550adb8ee7d81cd9a17c3d 🚀

--- index.636990ae.js
+++
@@ -1,2 +0,0 @@
-window.onscroll=function(){return e=document.querySelector("[data-header]"),t=e.offsetHeight,void(window.pageYOffset>t?e.classList.add("header--no-transparency"):e.classList.remove("header--no-transparency"));var e,t},document.querySelectorAll("a.site-nav__link").forEach((function(e){e.addEventListener("click",(function(e){e.preventDefault();var t=this.getAttribute("href").substring(1),n=document.getElementById(t).getBoundingClientRect().top-99;window.scrollBy({top:n,behavior:"smooth"})}))}));
-//# sourceMappingURL=index.636990ae.js.map
